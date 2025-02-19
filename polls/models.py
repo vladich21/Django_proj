d47d18881d076749b3873a4a6af7238fbe02c3e5
Владислав <77724398+vladich21@users.noreply.github.com>
2025-02-19 10:16:51 +0000

    lab 2

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.db import models
+from django.utils import timezone
+
+class SibTransUser(models.Model):
+    
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    # ...
+    def __str__(self):
+        return self.choice_text
